f41ac4c02f949a6ba86ce05faff18053a09bb041
duwilaja <apaaja.software@gmail.com>
2024-02-06 09:09:46 +0700

    change logo

--- index.spruha.php
+++ index.spruha.php
@@ -54,24 +54,24 @@ $menu="";
 					<div class="card">
 						<div class="row row-sm">
 							<div class="col-lg-6 col-xl-5 d-none d-lg-block text-center bg-primary details">
-								<div class="mt-5 pt-4 p-2 pos-absolute">
-									<img src="spruha/assets/img/brand/logo-light.png" class="d-lg-none header-brand-img text-start float-start mb-4 error-logo-light" alt="logo">
-									<img src="spruha/assets/img/brand/logo.png" class=" d-lg-none header-brand-img text-start float-start mb-4 error-logo" alt="logo">
+								<div class="mt-5 pt-4 p-2 pos-absolute" style="position:relative;">
+									<img src="img/polri.svg" class="d-lg-none header-brand-img text-start float-start mb-4 error-logo-light" alt="logo">
+									<img src="img/polri.svg" class=" d-lg-none header-brand-img text-start float-start mb-4 error-logo" alt="logo">
 									<div class="clearfix"></div>
-									<img src="spruha/assets/img/svgs/user.svg" class="ht-100 mb-0" alt="user">
-									<h5 class="mt-4 text-white">Create Your Account</h5>
-									<span class="tx-white-6 tx-13 mb-5 mt-xl-0">Signup to create, discover and connect with the global community</span>
+									<img src="img/polri.png" class="ht-200 mb-0" alt="user">
+									<h5 class="mt-4 text-white hidden">Create Your Account</h5>
+									<span class="tx-white-6 tx-13 mb-5 mt-xl-0 hidden">Signup to create, discover and connect with the global community</span>
 								</div>
 							</div>
 							<div class="col-lg-6 col-xl-7 col-xs-12 col-sm-12 login_form ">
 								<div class="main-container container-fluid">
 									<div class="row row-sm">
 										<div class="card-body mt-2 mb-2">
-											<img src="spruha/assets/img/brand/logo.png" class=" d-lg-none header-brand-img text-start float-start mb-4" alt="logo">
+											<img src="img/polri.svg" class=" d-lg-none header-brand-img text-start float-start mb-4" alt="logo">
 											<div class="clearfix"></div>
 											<form action="" method="POST" name="login_form" id="login_form">
 												<h5 class="text-start mb-2">Signin to Your Account</h5>
-												<p class="mb-4 text-muted tx-13 ms-0 text-start">Signin to create, discover and connect with the global community</p>
+												<p class="mb-4 text-muted tx-13 ms-0 text-start">&nbsp;</p>
 												<div class="form-group text-start">
 													<label>Username</label>
 													<input type="text" name="user" class="form-control" placeholder="" value="<?php echo $user?>">
